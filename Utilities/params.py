a8fd8256fd810c2804cbc65ef049e9e1100ee3fd
Marc Hansen <tsz841@alumni.ku.dk>
2023-09-30 12:28:11 +0200

    first work on params

---
+++ Utilities/params.py
@@ -0,0 +1,37 @@
+
+
+
+class Params():
+    def __init__(self):
+        self.iniciate_params()
+
+
+        self.params['tax'] = {'aktiesparekonto': 0.17,
+                              ''}
+
+
+
+
+
+
+
+
+
+    def iniciate_params(self):
+        self.params = dict()
+        self.params['tax'] = dict()
+        self.params['returns'] = dict()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
