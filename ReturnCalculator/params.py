86b1d979a105b26e2a1a1a16bf5d633a2aad058a
Marc Hansen <tsz841@alumni.ku.dk>
2023-09-30 20:42:54 +0200

    started on return_calculator

--- ReturnCalculator/params.py
+++ ReturnCalculator/params.py
@@ -7,12 +7,9 @@ class Params():
 
 
         self.params['tax'] = {'aktiesparekonto': 0.17,
-                              ''}
-
-
-
-
-
+                              'lav_aktieskat': 0.27,
+                              'high_aktieskat': 0.42}
+        self.params['return'] = 0.07
 
 
 
@@ -20,7 +17,7 @@ class Params():
     def iniciate_params(self):
         self.params = dict()
         self.params['tax'] = dict()
-        self.params['returns'] = dict()
+        #self.params['return'] = dict()
 
 
 
---
+++ ReturnCalculator/return_calculator.py
@@ -0,0 +1,10 @@
+from .params import Params
+
+
+
+class ReturnCalculator(Params):
+    def __init__(self, year, ):
+        Params.__init__(self)  
+        return
+    
+    
